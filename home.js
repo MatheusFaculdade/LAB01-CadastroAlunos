a3e601b7dc75f05ad40ebf0d96f12ab8765d105c
matheus-henrique <matheus.henrique@askblue.com>
2025-03-31 15:59:03 -0300

    exercicio 2 mudanças

--- home.js
+++ home.js
@@ -1,21 +1,41 @@
+class Aluno {
+    constructor(nome, idade, curso, notaFinal) {
+        this.nome = nome;
+        this.idade = idade;
+        this.curso = curso;
+        this.notaFinal = parseFloat(notaFinal);
+    }
+
+    isAprovado() {
+        return this.notaFinal >= 7;
+    }
+
+    toString() {
+        return `Nome: ${this.nome}, Idade: ${this.idade}, Curso: ${this.curso}, Nota Final: ${this.notaFinal}, Aprovado: ${this.isAprovado() ? 'Sim' : 'Não'}`;
+    }
+}
+
 let usuarios = [];
 const nomeInput = document.getElementById("nome");
 const idadeInput = document.getElementById("idade");
 const cursoInput = document.getElementById("curso");
 const notaFinalInput = document.getElementById("notaFinal");
-const cadastrarBtn = document.getElementById("cadastrarBtn");
 const corpoTabela = document.getElementById("bodytable");
 
+function cadastrar() {
+    const nome = nomeInput.value;
+    const idade = idadeInput.value;
+    const curso = cursoInput.value;
+    const notaFinal = notaFinalInput.value;
 
-function cadastrar(){
-    const usuario = {
-        nome: nomeInput.value,
-        idade: idadeInput.value,
-        curso: cursoInput.value,
-        notaFinal: notaFinalInput.value
-    };
+    if (!nome || !idade || !curso || !notaFinal) {
+        alert("Preencha todos os campos!");
+        return;
+    }
 
-    usuarios.push(usuario);
+    const aluno = new Aluno(nome, idade, curso, notaFinal);
+
+    usuarios.push(aluno);
 
     nomeInput.value = '';
     idadeInput.value = '';
@@ -27,34 +47,35 @@ function cadastrar(){
     alert("Aluno cadastrado com sucesso!");
 }
 
-function editarAluno(indice){
-    const usuario = usuarios[indice];
-    nomeInput.value = usuario.nome;
-    idadeInput.value = usuario.idade;
-    cursoInput.value = usuario.curso;
-    notaFinalInput.value = usuario.notaFinal;
+function editarAluno(indice) {
+    const aluno = usuarios[indice];
+    nomeInput.value = aluno.nome;
+    idadeInput.value = aluno.idade;
+    cursoInput.value = aluno.curso;
+    notaFinalInput.value = aluno.notaFinal;
 
     usuarios.splice(indice, 1);
 
     renderizarTabela();
 }
 
-function excluirAluno(indice){
-        usuarios.splice(indice, 1);
-        renderizarTabela();
-        alert("Aluno excluido com sucesso!");
+function excluirAluno(indice) {
+    usuarios.splice(indice, 1);
+    renderizarTabela();
+    alert("Aluno excluído com sucesso!");
 }
 
 function renderizarTabela() {
     corpoTabela.innerHTML = '';
-    
-    usuarios.forEach((usuario, indice) => {
+
+    usuarios.forEach((aluno, indice) => {
         const linha = document.createElement('tr');
         linha.innerHTML = `
-            <td>${usuario.nome}</td>
-            <td>${usuario.idade}</td>
-            <td>${usuario.curso}</td>
-            <td>${usuario.notaFinal}</td>
+            <td>${aluno.nome}</td>
+            <td>${aluno.idade}</td>
+            <td>${aluno.curso}</td>
+            <td>${aluno.notaFinal}</td>
+            <td>${aluno.isAprovado() ? 'Aprovado' : 'Reprovado'}</td>
             <td>
                 <button onclick="editarAluno(${indice})">Editar</button>
                 <button onclick="excluirAluno(${indice})">Excluir</button>
